3f15c77b328125c852e83261a0cee533af6909f2
Luka Skeledzija <36110818+lukaske@users.noreply.github.com>
2024-07-09 02:22:29 +0200

    frontend

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,19 +2,55 @@ import "@mantine/core/styles.css";
 import Head from "next/head";
 import { MantineProvider } from "@mantine/core";
 import { theme } from "../theme";
+import '@rainbow-me/rainbowkit/styles.css';
+import {
+  getDefaultConfig,
+  RainbowKitProvider,
+} from '@rainbow-me/rainbowkit';
+import { WagmiProvider } from 'wagmi';
+import {
+  mainnet,
+  polygon,
+  optimism,
+  arbitrum,
+  base,
+  taiko,
+  sepolia,
+} from 'wagmi/chains';
+import {
+  QueryClientProvider,
+  QueryClient,
+} from "@tanstack/react-query";
+import { sep } from "path";
+
 
 export default function App({ Component, pageProps }: any) {
+  const config = getDefaultConfig({
+    appName: 'REDOctogon',
+    projectId: 'YOUR_PROJECT_ID',
+    chains: [mainnet, polygon, taiko, sepolia],
+    ssr: true, // If your dApp uses server side rendering (SSR)
+  });
+  const queryClient = new QueryClient();
+
   return (
-    <MantineProvider theme={theme}>
-      <Head>
-        <title>Mantine Template</title>
-        <meta
-          name="viewport"
-          content="minimum-scale=1, initial-scale=1, width=device-width, user-scalable=no"
-        />
-        <link rel="shortcut icon" href="/favicon.svg" />
-      </Head>
-      <Component {...pageProps} />
-    </MantineProvider>
+    <WagmiProvider config={config}>
+    <QueryClientProvider client={queryClient}>
+      <RainbowKitProvider>
+          <MantineProvider theme={theme}>
+          <Head>
+            <title>Mantine Template</title>
+            <meta
+              name="viewport"
+              content="minimum-scale=1, initial-scale=1, width=device-width, user-scalable=no"
+            />
+            <link rel="shortcut icon" href="/favicon.svg" />
+          </Head>
+          <Component {...pageProps} />
+        </MantineProvider>
+      </RainbowKitProvider>
+    </QueryClientProvider>
+  </WagmiProvider>
+
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,40 @@
-import { Button, Group } from "@mantine/core";
+import { Button, Group, Container } from "@mantine/core";
+import { ConnectButton } from '@rainbow-me/rainbowkit';
+import { createPublicClient, http } from 'viem';
+import { mainnet } from 'viem/chains';
+import abi from '../contracts/verifier.abi.json';
 
 export default function IndexPage() {
+
+  const contractAddress = '0xYourContractAddressHere';
+  
+  const publicClient = createPublicClient({
+    chain: mainnet,
+    transport: http()
+  })
+
+  
+  
+
+  const testVerifier = async () => {
+    const data = await publicClient.readContract({
+      address: '0x5fb2559a81beff3864c907a49da8a2fd657693fc',
+      abi: ,
+      functionName: 'totalSupply',
+    })
+  
+  };
+
   return (
-    <Group mt={50} justify="center">
-      <Button size="xl">Welcome to Mantine!</Button>
-    </Group>
+    <Container fluid>
+      <Group mt={50} justify="center">
+        <ConnectButton />
+      </Group>
+      <Group mt={50} justify="center">
+        <Button onClick={testVerifier}>Test verifier</Button>  
+      </Group>
+
+
+    </Container>
   );
 }
