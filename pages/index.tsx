99506c6dd6e154c77fea4803def28d9d937b16c0
Luka Skeledzija <36110818+lukaske@users.noreply.github.com>
2024-07-09 09:47:39 +0200

    bounty contract

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,26 +4,7 @@ import { createPublicClient, http } from 'viem';
 import { mainnet } from 'viem/chains';
 import abi from '../contracts/verifier.abi.json';
 
-export default function IndexPage() {
-
-  const contractAddress = '0xYourContractAddressHere';
-  
-  const publicClient = createPublicClient({
-    chain: mainnet,
-    transport: http()
-  })
-
-  
-  
-
-  const testVerifier = async () => {
-    const data = await publicClient.readContract({
-      address: '0x5fb2559a81beff3864c907a49da8a2fd657693fc',
-      abi: ,
-      functionName: 'totalSupply',
-    })
-  
-  };
+export default function IndexPage() {  
 
   return (
     <Container fluid>
@@ -31,10 +12,8 @@ export default function IndexPage() {
         <ConnectButton />
       </Group>
       <Group mt={50} justify="center">
-        <Button onClick={testVerifier}>Test verifier</Button>  
+        <Button>Test verifier</Button>  
       </Group>
-
-
     </Container>
   );
 }
